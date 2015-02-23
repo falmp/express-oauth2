7705c6739717bf56d35b1c6b239e2b3ffb516997
falmp <chico.lopes@gmail.com>
2015-02-23 02:39:07 -0300

    Fixed login flow with flash messages and redirects

--- app.js
+++ app.js
@@ -89,6 +89,7 @@ app.use(session({
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
+
 app.use(function(req, res, next) {
     res.locals.user = req.user;
     res.locals.flash = req.flash();
--- routes/index.js
+++ routes/index.js
@@ -10,18 +10,31 @@ module.exports = function(passport) {
   
   router.get('/auth/logout', function(req, res, next) {
     req.logout();
+
+    req.flash('success', 'You are now logged out.');
+
     res.redirect('/');
   });
   
-  router.get('/auth/login', passport.authenticate('bioid', {
-    successFlash: 'Welcome!',
-    failureFlash: 'Ops!'
-  }));
+  router.get('/auth/login', passport.authenticate('bioid'));
   
   router.get('/auth/callback', passport.authenticate('bioid', {
-    successRedirect: '/',
-    failureRedirect: '/'
-  }));
+    failureRedirect: '/',
+    successFlash: 'Welcome, you are now logged in.',
+    failureFlash: true
+  }), function(req, res, next) {
+    var url = req.session.login_redirect || '/profile';
+
+    delete req.session.login_redirect;
+
+    debug('user is logged in and will be redirected to %s', url);
+
+    res.redirect(url);
+  });
+
+  router.get('/profile', isLoggedIn, function(req, res, next) {
+    res.render('profile');
+  });
 
   router.get('/protected', isLoggedIn, function(req, res, next) {
     res.render('protected');
@@ -36,5 +49,11 @@ var isLoggedIn = function(req, res, next) {
   if (req.isAuthenticated())
     return next();
   
+  debug('user will be redirected to %s', req.url);
+
+  req.flash('error', 'The page you are accessing is protected, please log in.');
+
+  req.session.login_redirect = req.url;
+
   res.redirect('/');
 }
