38b5abdeb1076c0d0b7eb4ebb39450f59f80a5e0
falmp <chico.lopes@gmail.com>
2015-02-22 23:04:09 -0300

    Added flash messages to template locals

--- app.js
+++ app.js
@@ -91,6 +91,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(function(req, res, next) {
     res.locals.user = req.user;
+    res.locals.flash = req.flash();
     next();
 });
 
